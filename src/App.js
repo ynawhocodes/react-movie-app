fdacaf3b7bb3fdb5513e027bdfd3cf009d1f0658
ynawhocodes <ynawhocodes@gmail.com>
2021-07-27 17:06:05 +0900

    use state

--- src/App.js
+++ src/App.js
@@ -1,49 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 
-function Food( {name, price, rating}) {
-  return (
-    <div>
-      <p>name: {name}</p>
-      <p>{price}</p>
-      <p>rating: {rating}</p>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  }
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}))
+  }
+  render() {
+    return (
+      <>
+      <p>number: {this.state.count}</p>
+      <button onClick={this.add}>+</button>
+      <button onClick={this.minus}>-</button>
+      </>
+    )
+  }
 }
-function App() {
-  return (
-    <div>
-      {foodList.map(props => (<Food key={props.id} name={props.name} price={props.price} rating={props.rating}/> ))}
-    </div>
-  );
-}
 
-const foodList = [
-  {
-    id: 1,
-    name: "apple",
-    price: 5000, 
-    rating: 4,
-  },
-  {
-    id: 2,
-    name: "banana",
-    price: 2000, 
-    rating: 3.5,
-  },
-  {
-    id: 3,
-    name: "lemon",
-    price: 10000, 
-    rating: 3,
-  },
 
-]
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  rating: PropTypes.number.isRequired,
-};
 
 export default App;
