5acd8f62f59f8a8f828c513406b19d76e4df96bc
ynawhocodes <ynawhocodes@gmail.com>
2021-07-27 16:19:13 +0900

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-function Food( {name, price}) {
+function Food( {name, price, rating}) {
   return (
     <div>
       <p>name: {name}</p>
       <p>{price}</p>
+      <p>rating: {rating}</p>
     </div>
   );
 }
 function App() {
   return (
     <div>
-      {foodList.map(props => (<Food key={props.id} name={props.name} price={props.price}/> ))}
+      {foodList.map(props => (<Food key={props.id} name={props.name} price={props.price} rating={props.rating}/> ))}
     </div>
   );
 }
@@ -24,17 +26,24 @@ const foodList = [
     rating: 4,
   },
   {
-    id: 1,
+    id: 2,
     name: "banana",
     price: 2000, 
     rating: 3.5,
   },
   {
-    id: 1,
+    id: 3,
     name: "lemon",
     price: 10000, 
     rating: 3,
   },
 
 ]
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
 export default App;
