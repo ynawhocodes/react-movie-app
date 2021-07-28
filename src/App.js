27f03d34014808dd94360ff3741a27301dcdbc61
ynawhocodes <ynawhocodes@gmail.com>
2021-07-28 11:13:23 +0900

    reset master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,4 @@
 import React from 'react';
 
 
-class App extends React.Component {
-  state = {
-    count: 0,
-  };
-
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  }
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}))
-  }
-  render() {
-    return (
-      <>
-      <p>number: {this.state.count}</p>
-      <button onClick={this.add}>+</button>
-      <button onClick={this.minus}>-</button>
-      </>
-    )
-  }
-}
-
 export default App;
