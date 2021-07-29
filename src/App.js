a70422e6e2782ea0b362d9259d72de28f9f595a8
ynawhocodes <ynawhocodes@gmail.com>
2021-07-29 10:25:04 +0900

    Home.js Home.css'

--- src/App.js
+++ src/App.js
@@ -1,47 +1,8 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import Home from './routes/Home';
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    isLoding: true,
-    movies: [],
-  };
-  
-  getMovies = async () => {
-    const { 
-      data: {
-          data: {movies},
-        },
-  } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    this.setState({ movies, isLoding : false });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  }
-  
-  render() {
-    const { isLoding, movies } = this.state;
-    return <section className="container">
-        {isLoding ? (
-          <div className="loader">
-            <span className="load__text">Loading</span>
-          </div>) : (
-          <div className="movies">{
-            movies.map((movie) => {
-              return <Movie key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-              genres={movie.genres}/>
-            })} 
-          </div>
-        )}
-    </section>  
-  }
+function App() {
+  return <Home/>;
 }
 export default App;
